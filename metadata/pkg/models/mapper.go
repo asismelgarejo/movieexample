6f0495bc41db887d2446c6736b6b1d14e2fe9e33
Asis <asismelgarejo@gmail.com>
2023-11-12 15:37:58 -0500

    protobuf gRPC services were created

---
+++ metadata/pkg/models/mapper.go
@@ -0,0 +1,23 @@
+package models
+
+import "movieexample.com/gen"
+
+// MetadataToProto translates a Metadata strict into a generated proto counterpart.
+func MetadataToProto(m *Metadata) *gen.Metadata {
+	return &gen.Metadata{
+		Id:          m.ID,
+		Title:       m.Title,
+		Description: m.Description,
+		Director:    m.Director,
+	}
+}
+
+// MetadataFromProto converts a proto counterpart to a Metadata struct
+func MetadataFromProto(m *gen.Metadata) Metadata {
+	return Metadata{
+		ID:          m.Id,
+		Title:       m.Title,
+		Description: m.Description,
+		Director:    m.Director,
+	}
+}
