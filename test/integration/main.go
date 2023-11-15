0f2e6c1dfa6e270d27c0960a50aa1ef6100c417b
Asis <asismelgarejo@gmail.com>
2023-11-15 15:31:55 -0500

    integration testing

---
+++ test/integration/main.go
@@ -0,0 +1,14 @@
+package main
+
+const (
+	metadataServiceName = "metadata"
+	ratingServiceName   = "rating"
+	movieServiceName    = "movie"
+	metadataServiceAddr = "localhost:8081"
+	ratingServiceAddr   = "localhost:8082"
+	movieServiceAddr    = "localhost:8083"
+)
+
+func main() {
+
+}
